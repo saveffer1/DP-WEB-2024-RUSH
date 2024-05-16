ec6622ced39be51954b8562236b3aec3f6a70034
Wiraphat Prasomphong <saveffer01@gmail.com>
2024-05-16 12:25:17 +0700

    initail commit

---
+++ js/tailwind.js
@@ -0,0 +1,26 @@
+const tailwindcdnlink = "https://cdn.tailwindcss.com";
+
+function initTailwindScript(source){
+    const tailwindScript = document.createElement('script');
+    tailwindScript.src = source;
+    return tailwindScript;
+}
+
+// tailwind configuration
+tailwindScript = initTailwindScript(tailwindcdnlink);
+tailwindScript.onload = function(){
+    // add custom tailwind configuration here
+    window.tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    clifford: '#00ff00',
+                }
+            }
+        }
+    }
+
+    console.log("Tailwind CSS loaded successfully");
+}
+
+document.head.appendChild(tailwindScript);
